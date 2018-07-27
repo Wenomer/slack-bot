bb590c05aa91441fe0a6ed4adaf6579833ba3e64
Alex Schastiy <alexey.schastniy@weavora.com>
2018-07-27 10:48:04 +0300

    test

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -45,6 +45,32 @@ class ApiController extends Controller
             return $this->jsonResponse(['challenge' => $this->getPost($request, 'challenge')]);
         }
 
+        if ($request->getContent() && $this->getPost($request, 'command') == '/lunch') {
+
+            $text = [
+                'text' => "Would you like to play a game?",
+                "attachments" => [
+                    [
+                        "text" => "Choose a game to play",
+                        "fallback" => "You are unable to choose a game",
+                        "callback_id" => "wopr_game",
+                        "color" => "#3AA3E3",
+                        "attachment_type" => "default",
+                        "actions" => [
+                            [
+                                "name" => "game",
+                                "text" => "Chess",
+                                "type" => "button",
+                                "value" => "chess"
+                            ]
+                        ]
+                    ]
+                ],
+            ];
+
+            return $this->jsonResponse($text);
+        }
+
         return new Response('test');
     }
 }
