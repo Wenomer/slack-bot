6db748f6dc2b74ab0eb872dae011a53b9c1cc4cc
Alex Schastiy <alexey.schastniy@weavora.com>
2018-08-15 12:23:23 +0300

    test

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -144,7 +144,7 @@ class ApiController extends Controller
             'debug' => TRUE,
             'form_params' => [
                 'token' => 'xoxb-329104271632-tOhuGbOBQpCiydd2gnldkKMl',
-                'channel' => 'U1KA3F420',
+                'channel' => 'D9PRWHVQD',
                 'text' => 'hello',
                 'as_user' => false
             ],
