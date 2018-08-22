5f7ba46df4b9c671e51d071952ff1d111c89d5e2
Alex Schastiy <alexey.schastniy@weavora.com>
2018-08-22 09:38:23 +0300

    test

--- react.php
+++ react.php
@@ -10,10 +10,10 @@ $server = new React\Http\Server(function (Psr\Http\Message\ServerRequestInterfac
         "Hello World!\n"
     );
 });
-
-$socket = new React\Socket\Server(9000, $loop);
+$port = getenv('PORT');
+$socket = new React\Socket\Server($port, $loop);
 $server->listen($socket);
 
-echo "Server running at http://127.0.0.1:8000\n";
+echo "Server running at http://127.0.0.1:{$port}\n";
 
 $loop->run();
